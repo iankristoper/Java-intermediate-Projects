00afbb8601a2b5d8273c18cb281f0e4734ea0c12
iankristoper <iankristoper26@gmail.com>
2024-08-14 22:08:10 +0800

    initial commit

---
+++ OnlineShoppingSystem/src/main/java/com/ian/onlineshoppingsystem/OnlineShoppingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ian.onlineshoppingsystem;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+public class OnlineShoppingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
